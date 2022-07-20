7e882bcde7ef5b72b42e56c622509b0734ac69e2
Jonathan Tonglet <jonathan.tonglet@gmail.com>
2022-07-20 11:43:27 +0200

    add IMDb loading

--- huggingface_loading_script.py
+++ huggingface_loading_script.py
@@ -17,15 +17,10 @@ def huggingface_to_csv(name,
         pd.DataFrame(dataset['train']).to_csv(output_path + 'train.csv',index=False)
         pd.DataFrame(dataset['test']).to_csv(output_path + 'test.csv',index=False)
 
-    
-
-
-
-
-
 huggingface_to_csv('silicone','dyda_e','datasets/sentiment/emotion/silicone/')
 huggingface_to_csv('liar','default','datasets/fake_news/liar/')
 huggingface_to_csv('emotion','default','datasets/sentiment/emotion/CARER/')
+huggingface_to_csv('imdb','plain_text','datasets/sentiment/emotion/IMDb/')
 huggingface_to_csv('ag_news','default','datasets/topic/ag_news/')
 huggingface_to_csv('yelp_polarity','plain_text','datasets/sentiment/polarity/yelp/')
 huggingface_to_csv('sst2','default','datasets/sentiment/polarity/SST2/')
