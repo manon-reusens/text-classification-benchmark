d4f9f4b07d442a104b8895a2699c9b741c0ab150
Jonathan Tonglet <jonathan.tonglet@gmail.com>
2022-10-02 12:53:18 +0200

    updated best parameter values

--- config/best_params.py
+++ config/best_params.py
@@ -17,30 +17,40 @@ where the first-level keys correspond to the dataset names, and the sub-keys cor
 best_params = {}
 
 
-#to do : xgb
+#Done
 best_params['gossipcop'] = {'tfidf_lr':LogisticRegression(C=8.959,random_state=SEED),
                      'ft_lr':LogisticRegression(C=7.346,random_state=SEED),
                      'tfidf_rf':RandomForestClassifier(n_estimators=44,max_features='sqrt',random_state=SEED),
                      'ft_rf':RandomForestClassifier(n_estimators=94,max_features='sqrt',random_state=SEED),
                      'tfidf_svm':SVC(probability=True,kernel='linear',C=1.145,random_state=SEED),
                      'ft_svm':SVC(probability=True,kernel='rbf',C=8.34,random_state=SEED),
-                     'tfidf_xgb':XGBClassifier(gamma=,learning_rate=,max_depth=,n_estimators=,random_state=SEED),
-                     'ft_xgb':XGBClassifier(gamma=,learning_rate=,max_depth=,n_estimators=,random_state=SEED)
+                     'tfidf_xgb':XGBClassifier(gamma=0.6192,learning_rate=0.0642,max_depth=9,n_estimators=149,random_state=SEED),
+                     'ft_xgb':XGBClassifier(gamma=0.8574,learning_rate=0.0987,max_depth=10,n_estimators=149,random_state=SEED)
                            }
 
-#to do : b
+#Done
+best_params['CoAID'] = {'tfidf_lr':LogisticRegression(C=7.127,random_state=SEED),
+                     'ft_lr':LogisticRegression(C=8.93,random_state=SEED),
+                     'tfidf_rf':RandomForestClassifier(n_estimators=98,max_features='log2',random_state=SEED),
+                     'ft_rf':RandomForestClassifier(n_estimators=107,max_features='sqrt',random_state=SEED),
+                     'tfidf_svm':SVC(probability=True,kernel='linear',C=9.2,random_state=SEED),
+                     'ft_svm':SVC(probability=True,kernel='rbf',C=4.871,random_state=SEED),
+                     'tfidf_xgb':XGBClassifier(gamma=0.9251,learning_rate=0.0969,max_depth=4,n_estimators=115,random_state=SEED),
+                     'ft_xgb':XGBClassifier(gamma=0.4993,learning_rate=0.0147,max_depth=7,n_estimators=95,random_state=SEED)
+                      }
+#Done
 best_params['liar'] = {'tfidf_lr':LogisticRegression(C=4.376,random_state=SEED),
                      'ft_lr':LogisticRegression(C=7.82,random_state=SEED),
                      'tfidf_rf':RandomForestClassifier(n_estimators=121,max_features='sqrt',random_state=SEED),
-                     'ft_rf':RandomForestClassifier(n_estimators=94,max_features='log2',random_state=SEED)
+                     'ft_rf':RandomForestClassifier(n_estimators=94,max_features='log2',random_state=SEED),
                      'tfidf_svm':SVC(probability=True,kernel='linear',C=2.956,random_state=SEED),
                      'ft_svm':SVC(probability=True,kernel='rbf',C=9.637,random_state=SEED),
-                     'tfidf_xgb':XGBClassifier(gamma=,learning_rate=,max_depth=,n_estimators=,random_state=SEED),
-                     'ft_xgb':XGBClassifier(gamma=,learning_rate=,max_depth=,n_estimators=,random_state=SEED)
+                     'tfidf_xgb':XGBClassifier(gamma=0.3576,learning_rate=0.08316,max_depth=7,n_estimators=149,random_state=SEED),
+                     'ft_xgb':XGBClassifier(gamma=0.2985,learning_rate=0.0772,max_depth=3,n_estimators=134,random_state=SEED)
                       }
 
 
-#done
+#Done
 best_params['tweetEval'] = {'tfidf_lr':LogisticRegression(C=5.064,random_state=SEED),
                          'ft_lr':LogisticRegression(C=2.11,random_state=SEED),
                             'tfidf_rf':RandomForestClassifier(n_estimators=187,max_features='log2',random_state=SEED),
@@ -66,32 +76,32 @@ best_params['CARER'] = {'tfidf_lr':LogisticRegression(C=8.9,random_state=SEED),
 best_params['silicone'] = {'tfidf_lr':LogisticRegression(C=6.277,random_state=SEED),
                      'ft_lr':LogisticRegression(C=8.173,random_state=SEED),
                      'tfidf_rf':RandomForestClassifier(n_estimators=93,max_features='sqrt',random_state=SEED),
-                     'ft_rf':RandomForestClassifier(n_estimators=191,max_features='sqrt',random_state=SEED)
+                     'ft_rf':RandomForestClassifier(n_estimators=191,max_features='sqrt',random_state=SEED),
                      'tfidf_svm':LinearSVC(C=0.6963,loss='hinge',random_state=SEED),
                      'ft_svm':LinearSVC(C=9.853,loss='hinge',random_state=SEED),
                      'tfidf_xgb':XGBClassifier(gamma=0.041,learning_rate=0.0767,max_depth=10,n_estimators=150,random_state=SEED),
-                     'ft_xgb':XGBClassifier(gamma=,learning_rate=,max_depth=,n_estimators=,random_state=SEED)
+                     'ft_xgb':XGBClassifier(gamma=0.2448,learning_rate=0.0529,max_depth=9,n_estimators=141,random_state=SEED)
                       }
 
 #Done
 best_params['imdb'] = {'tfidf_lr':LogisticRegression(C=8.99,random_state=SEED),
                      'ft_lr':LogisticRegression(C=9.752,random_state=SEED),
                      'tfidf_rf':RandomForestClassifier(n_estimators=181,max_features='log2',random_state=SEED),
-                     'ft_rf':RandomForestClassifier(n_estimators=119,max_features='sqrt',random_state=SEED)
+                     'ft_rf':RandomForestClassifier(n_estimators=119,max_features='sqrt',random_state=SEED),
                      'tfidf_svm':LinearSVC(C=0.4577,loss='hinge',random_state=SEED),
                      'ft_svm':LinearSVC(C=9.328, loss='hinge',random_state=SEED),
                         'tfidf_xgb':XGBClassifier(gamma=0.5457,learning_rate=0.0762,max_depth=7,n_estimators=148,random_state=SEED),
                      'ft_xgb':XGBClassifier(gamma=0.2529,learning_rate=0.0998,max_depth=5,n_estimators=103,random_state=SEED)
                       }
 
-#
+#TO do random forests and  ft xgb
 best_params['yelp'] = {'tfidf_lr':LogisticRegression(C=3.327,random_state=SEED),
                      'ft_lr':LogisticRegression(C=8.537,random_state=SEED),
                      'tfidf_rf':RandomForestClassifier(random_state=SEED),
-                     'ft_rf':RandomForestClassifier(random_state=SEED)
+                     'ft_rf':RandomForestClassifier(random_state=SEED),
                      'tfidf_svm':LinearSVC(C=0.4577,loss='hinge',random_state=SEED),
                      'ft_svm':LinearSVC(C=6.779, loss='hinge',random_state=SEED),
-                        'tfidf_xgb':XGBClassifier(gamma=0.2416,learning_rate=0.0837,max_depth=9,n_estimators=100,random_state=SEED),
+                     'tfidf_xgb':XGBClassifier(gamma=0.2416,learning_rate=0.0837,max_depth=9,n_estimators=100,random_state=SEED),
                      'ft_xgb':XGBClassifier(gamma=,learning_rate=,max_depth=,n_estimators=,random_state=SEED)
                       }
 
@@ -99,7 +109,7 @@ best_params['yelp'] = {'tfidf_lr':LogisticRegression(C=3.327,random_state=SEED),
 best_params['sst2'] = {'tfidf_lr':LogisticRegression(C=3.833,random_state=SEED),
                      'ft_lr':LogisticRegression(C=9.278,random_state=SEED),
                      'tfidf_rf':RandomForestClassifier(n_estimators=113,max_features='log2',random_state=SEED),
-                     'ft_rf':RandomForestClassifier(n_estimators=149,max_features='sqrt',random_state=SEED)
+                     'ft_rf':RandomForestClassifier(n_estimators=149,max_features='sqrt',random_state=SEED),
                      'tfidf_svm':LinearSVC(C=0.2439,loss='hinge',random_state=SEED),
                      'ft_svm':LinearSVC(C=6.517, loss='hinge',random_state=SEED),
                         'tfidf_xgb':XGBClassifier(gamma=0.6444,learning_rate=0.0648,max_depth=9,n_estimators=150,random_state=SEED),
@@ -110,7 +120,7 @@ best_params['sst2'] = {'tfidf_lr':LogisticRegression(C=3.833,random_state=SEED),
 best_params['iSarcasm'] = {'tfidf_lr':LogisticRegression(C=7.976,random_state=SEED),
                      'ft_lr':LogisticRegression(C=9.278,random_state=SEED),
                      'tfidf_rf':RandomForestClassifier(n_estimators=139,max_features='sqrt',random_state=SEED),
-                     'ft_rf':RandomForestClassifier(n_estimators=54,max_features='log2',random_state=SEED)
+                     'ft_rf':RandomForestClassifier(n_estimators=54,max_features='log2',random_state=SEED),
                      'tfidf_svm':SVC(kernel='linear',C=2.87,probability=True,random_state=SEED),
                      'ft_svm':SVC(kernel='rbf',C=9.08,probability=True,random_state=SEED),
                      'tfidf_xgb':XGBClassifier(gamma=0.528,learning_rate=0.0727,max_depth=9,n_estimators=148,random_state=SEED),
@@ -121,29 +131,29 @@ best_params['iSarcasm'] = {'tfidf_lr':LogisticRegression(C=7.976,random_state=SE
 best_params['SemEvalA'] = {'tfidf_lr':LogisticRegression(C=1.898,random_state=SEED),
                      'ft_lr':LogisticRegression(C=4.93,random_state=SEED),
                      'tfidf_rf':RandomForestClassifier(n_estimators=62,max_features='sqrt',random_state=SEED),
-                     'ft_rf':RandomForestClassifier(n_estimators=100,max_features='log2',random_state=SEED)
+                     'ft_rf':RandomForestClassifier(n_estimators=100,max_features='log2',random_state=SEED),
                      'tfidf_svm':SVC(kernel='linear',C=4.489,probability=True,random_state=SEED),
                      'ft_svm':SVC(kernel='rbf',C=3.143,probability=True,random_state=SEED),
                        'tfidf_xgb':XGBClassifier(gamma=0.8192,learning_rate=0.06548,max_depth=10,n_estimators=138,random_state=SEED),
                      'ft_xgb':XGBClassifier(gamma=0.9532,learning_rate=0.0551,max_depth=5,n_estimators=67,random_state=SEED)
                       }
 
-#to do all
-best_params['SARC'] = {'tfidf_lr':LogisticRegression(random_state=SEED),
-                     'ft_lr':LogisticRegression(random_state=SEED),
+#to do all (see google colab)
+best_params['SARC'] = {'tfidf_lr':LogisticRegression(C=1.4,random_state=SEED),
+                     'ft_lr':LogisticRegression(C=4.754,random_state=SEED),
                      'tfidf_rf':RandomForestClassifier(random_state=SEED),
-                     'ft_rf':RandomForestClassifier(random_state=SEED)
-                     'tfidf_svm':LinearSVC(random_state=SEED),
-                     'ft_svm':LinearSVC(random_state=SEED),
+                     'ft_rf':RandomForestClassifier(n_estimators=175,max_features='sqrt',random_state=SEED),
+                     'tfidf_svm':LinearSVC(C=0.7808,loss='hinge',random_state=SEED),
+                     'ft_svm':LinearSVC(C=6.9, loss='hinge',random_state=SEED),
                      'tfidf_xgb':XGBClassifier(random_state=SEED),
                      'ft_xgb':XGBClassifier(random_state=SEED)
                       }
 
-#to do XGB
+#Done
 best_params['twentynews'] = {'tfidf_lr':LogisticRegression(C=3.86,random_state=SEED),
                      'ft_lr':LogisticRegression(C=8.4,random_state=SEED),
                      'tfidf_rf':RandomForestClassifier(n_estimators=182,max_features='log2',random_state=SEED),
-                     'ft_rf':RandomForestClassifier(n_estimators=127,max_features='sqrt',random_state=SEED)
+                     'ft_rf':RandomForestClassifier(n_estimators=127,max_features='sqrt',random_state=SEED),
                      'tfidf_svm':SVC(kernel='rbf',C=6.245,probability=True,random_state=SEED),
                      'ft_svm':SVC(kernel='rbf',C=4.682,probability=True,random_state=SEED),
                      'tfidf_xgb':XGBClassifier(gamma=0.06291,learning_rate=0.05459,max_depth=8,n_estimators=113,random_state=SEED),
@@ -154,7 +164,7 @@ best_params['twentynews'] = {'tfidf_lr':LogisticRegression(C=3.86,random_state=S
 best_params['agnews'] = {'tfidf_lr':LogisticRegression(C=1.114,random_state=SEED),
                      'ft_lr':LogisticRegression(C=3.741,random_state=SEED),
                      'tfidf_rf':RandomForestClassifier(n_estimators=188,max_features='log2',random_state=SEED),
-                     'ft_rf':RandomForestClassifier(n_estimators=167,max_features='log2',random_state=SEED)
+                     'ft_rf':RandomForestClassifier(n_estimators=167,max_features='log2',random_state=SEED),
                      'tfidf_svm':LinearSVC(C=1.843,loss='hinge',random_state=SEED),
                      'ft_svm':LinearSVC(C=9.337,loss='hinge',random_state=SEED),
                      'tfidf_xgb':XGBClassifier(gamma=,learning_rate=,max_depth=,n_estimators=,random_state=SEED),
@@ -165,9 +175,9 @@ best_params['agnews'] = {'tfidf_lr':LogisticRegression(C=1.114,random_state=SEED
 best_params['yahoo'] = {'tfidf_lr':LogisticRegression(C=5.9,random_state=SEED),
                      'ft_lr':LogisticRegression(C=2.671,random_state=SEED),
                      'tfidf_rf':RandomForestClassifier(random_state=SEED),
-                     'ft_rf':RandomForestClassifier(random_state=SEED)
-                     'tfidf_svm':LinearSVC(random_state=SEED),
-                     'ft_svm':LinearSVC(random_state=SEED),
+                     'ft_rf':RandomForestClassifier(random_state=SEED),
+                     'tfidf_svm':LinearSVC(C=5.147,loss='hinge',random_state=SEED),
+                     'ft_svm':LinearSVC(C=,loss='hinge',random_state=SEED),
                      'tfidf_xgb':XGBClassifier(gamma=,learning_rate=,max_depth=,n_estimators=,random_state=SEED),
                      'ft_xgb':XGBClassifier(gamma=,learning_rate=,max_depth=,n_estimators=,random_state=SEED)
                       }
