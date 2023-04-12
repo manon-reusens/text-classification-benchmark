9ae2de50caffe0d0463017e3c002cdd0ea1aa90e
manon-reusens <101716925+manon-reusens@users.noreply.github.com>
2023-04-12 18:31:53 +0200

    Update fake_news_gossipcop.py

--- py_files/CNN_opt/fake_news_gossipcop.py
+++ py_files/CNN_opt/fake_news_gossipcop.py
@@ -5,7 +5,7 @@ import wandb
 from wandb.keras import WandbCallback
 import pandas as pd 
 sys.path.append(os.getcwd())
-os.environ['WANDB_DIR']= '/lustre1/scratch/344/vsc34470/wandb/'
+os.environ['WANDB_DIR']= 'wandb'
 
 #Load linguistic resources 
 from nltk import download
