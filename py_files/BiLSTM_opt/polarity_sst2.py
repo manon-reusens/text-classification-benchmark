ef380449b27ed3dc77ffac7b03bea5b0b145f7d6
manon-reusens <101716925+manon-reusens@users.noreply.github.com>
2023-04-12 17:03:40 +0200

    Update polarity_sst2.py

--- py_files/BiLSTM_opt/polarity_sst2.py
+++ py_files/BiLSTM_opt/polarity_sst2.py
@@ -30,7 +30,7 @@ download('wordnet',quiet=True,download_dir='/data/leuven/344/vsc34470/text-class
 SEED=int(sys.argv[1])
 tf.random.set_seed(SEED)
 seed(SEED)
-path= 'PATH'
+path= 'results/bilstm/'
 
 dl = DataLoader(['polarity'])
 data = dl.load()
