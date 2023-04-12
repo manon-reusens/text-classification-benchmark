5a389b4d142f27af34f08bbe695f7e1ee591723e
manon-reusens <101716925+manon-reusens@users.noreply.github.com>
2023-04-12 18:14:23 +0200

    Update tfidf_fakenews_gossipcop.py

--- py_files/LR_opt/tfidf_fakenews_gossipcop.py
+++ py_files/LR_opt/tfidf_fakenews_gossipcop.py
@@ -1,9 +1,9 @@
 import wandb
 import nltk
-download('stopwords',quiet=True)
-download('omw-1.4',quiet=True)
-download('punkt',quiet=True)
-download('wordnet',quiet=True)
+nltk.download('stopwords',quiet=True)
+nltk.download('omw-1.4',quiet=True)
+nltk.download('punkt',quiet=True)
+nltk.download('wordnet',quiet=True);
 
 
 import os
