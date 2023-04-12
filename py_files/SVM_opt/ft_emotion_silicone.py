d5a905f8525bd12687f7ea79933cb5b8a31a3d82
manon-reusens <101716925+manon-reusens@users.noreply.github.com>
2023-04-12 17:29:07 +0200

    Update ft_emotion_silicone.py

--- py_files/SVM_opt/ft_emotion_silicone.py
+++ py_files/SVM_opt/ft_emotion_silicone.py
@@ -31,7 +31,7 @@ os.environ["WANDB_SILENT"] = "true"
 SEED=int(sys.argv[1])
 OPT_ITER=7
 
-path= '/lustre1/scratch/344/vsc34470/results/svm/'
+path= 'results/svm/'
 dl = DataLoader(['emotion'])
 data = dl.load()
 
