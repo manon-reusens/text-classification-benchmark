b35efe6321256359f87f9caa909de4242fdd1d5f
manon-reusens <101716925+manon-reusens@users.noreply.github.com>
2023-04-12 17:15:08 +0200

    Update tfidf_moviereview_lr.py

--- py_files/LR_opt/tfidf_moviereview_lr.py
+++ py_files/LR_opt/tfidf_moviereview_lr.py
@@ -35,7 +35,7 @@ import yaml
 SEED=int(sys.argv[1])
 OPT_ITER=7
 
-path= '/lustre1/scratch/344/vsc34470/results/lr/' 
+path= 'results/logistic_regression/'
 dl = DataLoader(['polarity'])
 data = dl.load()
 
