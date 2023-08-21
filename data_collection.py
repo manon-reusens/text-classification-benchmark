97e7bf161b36952633bd3ea7fa09abf3ee41ce9e
manon-reusens <101716925+manon-reusens@users.noreply.github.com>
2023-08-21 14:40:53 +0200

    Update data_collection.py

--- data_collection.py
+++ data_collection.py
@@ -35,22 +35,34 @@ if not os.path.exists('fake_news'):
         os.makedirs('fake_news/CoAID')
     if not os.path.exists('fake_news/Gossipcop'):
         os.makedirs('fake_news/Gossipcop')
+    if not os.path.exists('fake_news/Politifact'):
+        os.makedirs('fake_news/Politifact')
+    if not os.path.exists('fake_news/McIntyre'):
+        os.makedirs('fake_news/McIntyre')
 if not os.path.exists('topic'):
     os.makedirs('topic/agnews')
     os.makedirs('topic/WOS')
+    os.makedirs('topic/bbc')
+    os.makedirs('topic/yahoo')
 if not os.path.exists('sentiment'):
     if not os.path.exists('sentiment/emotion'):
       os.makedirs('sentiment/emotion/tweetEval')
       os.makedirs('sentiment/emotion/CARER')
       os.makedirs('sentiment/emotion/silicone')
+      os.makedirs('sentiment/emotion/MELD')
+      #os.makedirs('sentiment/emotion/go_emotions')
     if not os.path.exists('sentiment/polarity'):
       os.makedirs('sentiment/polarity/imdb')
       os.makedirs('sentiment/polarity/movie_review')
       os.makedirs('sentiment/polarity/sst2')
+      os.makedirs('sentiment/polarity/CR')
+      os.makedirs('sentiment/polarity/STS-Gold')
     if not os.path.exists('sentiment/sarcasm'):
       os.makedirs('sentiment/sarcasm/SemEval')
       os.makedirs('sentiment/sarcasm/iSarcasm')
       os.makedirs('sentiment/sarcasm/SNH')
+      os.makedirs('sentiment/sarcasm/sarcasm_v2')
+      os.makedirs('sentiment/sarcasm/sarcasm_v1')
 
 #GitHub datasets
 #TweetEval Emotion
@@ -60,6 +72,14 @@ pd.read_table('https://raw.githubusercontent.com/cardiffnlp/tweeteval/main/datas
 pd.read_table('https://raw.githubusercontent.com/cardiffnlp/tweeteval/main/datasets/emotion/val_labels.txt').to_csv('sentiment/emotion/tweetEval/val_labels.csv',index=False)
 pd.read_table('https://raw.githubusercontent.com/cardiffnlp/tweeteval/main/datasets/emotion/test_text.txt').to_csv('sentiment/emotion/tweetEval/test_text.csv',index=False)
 pd.read_table('https://raw.githubusercontent.com/cardiffnlp/tweeteval/main/datasets/emotion/test_labels.txt').to_csv('sentiment/emotion/tweetEval/test_labels.csv',index=False)
+#MELD
+pd.read_csv('https://raw.githubusercontent.com/declare-lab/MELD/master/data/MELD/train_sent_emo.csv').to_csv('sentiment/emotion/MELD/train.csv',index=False)
+pd.read_csv('https://raw.githubusercontent.com/declare-lab/MELD/master/data/MELD/dev_sent_emo.csv').to_csv('sentiment/emotion/MELD/dev.csv',index=False)
+pd.read_csv('https://raw.githubusercontent.com/declare-lab/MELD/master/data/MELD/test_sent_emo.csv').to_csv('sentiment/emotion/MELD/test.csv',index=False)
+#GoEmotions
+#pd.read_table('https://raw.githubusercontent.com/google-research/google-research/master/goemotions/data/train.tsv', sep='\t').to_csv('sentiment/emotion/go_emotions/train.csv',index=False)
+#pd.read_table('https://raw.githubusercontent.com/google-research/google-research/master/goemotions/data/dev.tsv', sep='\t').to_csv('sentiment/emotion/go_emotions/dev.csv',index=False)
+#pd.read_table('https://raw.githubusercontent.com/google-research/google-research/master/goemotions/data/test.tsv', sep='\t').to_csv('sentiment/emotion/go_emotions/test.csv',index=False)
 #SemEval
 pd.read_table('https://raw.githubusercontent.com/Cyvhee/SemEval2018-Task3/master/datasets/train/SemEval2018-T3-train-taskA.txt').to_csv('sentiment/sarcasm/SemEval/train-taskA.csv',index=False)
 # pd.read_table('https://raw.githubusercontent.com/Cyvhee/SemEval2018-Task3/master/datasets/train/SemEval2018-T3-train-taskB.txt').to_csv('sentiment/sarcasm/SemEval/train-taskB.csv',index=False)
@@ -88,13 +108,20 @@ tar_dataset = "https://www.cs.cornell.edu/people/pabo/movie-review-data/rt-polar
 ftpstream = urllib.request.urlopen(tar_dataset)
 tar_dataset = tarfile.open(fileobj=ftpstream, mode="r|gz")
 tar_dataset.extractall(path='sentiment/polarity/movie_review')
-
 #Gossipcop
 fake=pd.read_csv('https://raw.githubusercontent.com/KaiDMML/FakeNewsNet/master/dataset/gossipcop_fake.csv')
 real=pd.read_csv('https://raw.githubusercontent.com/KaiDMML/FakeNewsNet/master/dataset/gossipcop_real.csv')
 fake.to_csv('fake_news/Gossipcop/fake.csv',index=False)
 real.to_csv('fake_news/Gossipcop/real.csv',index=False)
-            
+#Politifact
+fake=pd.read_csv('https://raw.githubusercontent.com/KaiDMML/FakeNewsNet/master/dataset/politifact_fake.csv')
+real=pd.read_csv('https://raw.githubusercontent.com/KaiDMML/FakeNewsNet/master/dataset/politifact_real.csv')
+fake.to_csv('fake_news/Politifact/fake.csv',index=False)
+real.to_csv('fake_news/Politifact/real.csv',index=False)
+#mcintyre
+pd.read_csv('https://raw.githubusercontent.com/GeorgeMcIntire/fake_real_news_dataset/main/fake_and_real_news_dataset.csv').to_csv('fake_news/McIntire/fake_or_real_news.csv',index=False)
+
+
 
 #Archives
 #SARC
@@ -107,12 +134,14 @@ huggingface_to_csv('silicone','dyda_e','sentiment/emotion/silicone/')
 huggingface_to_csv('liar','default','fake_news/liar/')
 huggingface_to_csv('emotion','default','sentiment/emotion/CARER/')
 huggingface_to_csv('imdb','plain_text','sentiment/polarity/imdb/')
+huggingface_to_csv('SetFit/CR', 'default','sentiment/polarity/CR/')
 huggingface_to_csv('ag_news','default','topic/agnews/')
-#huggingface_to_csv('yahoo_answers_topics','yahoo_answers_topics','topic/yahoo/')
+huggingface_to_csv('yahoo_answers_topics','yahoo_answers_topics','topic/yahoo/')
 #huggingface_to_csv('yelp_polarity','plain_text','sentiment/polarity/yelp/')
 huggingface_to_csv('sst2','default','sentiment/polarity/sst2/')
 huggingface_to_csv('raquiba/Sarcasm_News_Headline', 'default','sentiment/sarcasm/SNH/')
 huggingface_to_csv('web_of_science',  'WOS11967', 'topic/WOS/')
+huggingface_to_csv('SetFit/bbc-news', 'default' , 'topic/bbc/')
 
 
 #Return to root directory
